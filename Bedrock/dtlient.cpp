0504c1fb38b28b8da467330eb39d5deb0db1ec5a
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-10-17 09:59:32 +0200

    Create dtlient.cpp

---
+++ Bedrock/dtlient.cpp
@@ -0,0 +1,13 @@
+#include <Windows.h>
+#include <tchar.h>
+#include <libloaderapi.h>
+#include <wchar.h>
+#include "../MyDLL/Header.h"
+
+WINAPI DllMain(
+    HINSTANCE hinstDLL,  // handle to DLL module
+    DWORD fdwReason,     // reason for calling function
+    LPVOID lpReserved )  // reserved
+{
+  
+}
