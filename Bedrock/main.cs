3f1efd7ced3c8c0e5a4d5df7709f2492b34acfe7
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-10-20 19:58:42 +0200

    Add files via upload

--- Bedrock/main.cs
+++ Bedrock/main.cs
@@ -53,19 +53,19 @@ namespace DTinjector
 			string userprofile = System.Environment.GetEnvironmentVariable("USERPROFILE");
 			string tempdir = System.Environment.GetEnvironmentVariable("TEMP");
 			
-			// string executablePath = Assembly.GetEntryAssembly().Location;
-			// Icon theIcon = ExtractIconFromFilePath(executablePath);
-			// if (theIcon != null && !File.Exists(tempdir+"\\dtclienticon.ico"))
-			// {
-				// using (FileStream stream = new FileStream(tempdir+"\\dtclienticon.ico.ico", FileMode.CreateNew))
-				// {
-					// theIcon.Save(stream);
-				// }
-			// }
+			string executablePath = Assembly.GetEntryAssembly().Location;
+			Icon theIcon = ExtractIconFromFilePath(executablePath);
+			if (theIcon != null && !File.Exists(tempdir+"\\dtclienticon.ico"))
+			{
+				using (FileStream stream = new FileStream(tempdir+"\\dtclienticon.ico", FileMode.CreateNew))
+				{
+					theIcon.Save(stream);
+				}
+			}
 			this.BackColor = Color.FromArgb(0, 0, 0);
             this.Name = "DTclient Injector";
             this.Text = "DTclient Injector";
-			// this.Icon = new Icon(tempdir+"\\dtclienticon.ico");
+			this.Icon = new Icon(tempdir+"\\dtclienticon.ico");
             this.Size = new Size(460, 300);
 			this.MinimumSize = new Size(460, 300);
 			this.MaximumSize = new Size(460, 300);
@@ -76,7 +76,8 @@ namespace DTinjector
 			//
 			dsicon = new PictureBox();
             dsicon.Name = "dtclienticon";
-			dsicon.ImageLocation = tempdir+"\\dtclienticon.ico";
+			Download("https://github.com/DuckpvpTeam/DTclient/blob/main/banner.png", tempdir+"\\dtclientbanner.png");
+			dsicon.ImageLocation = tempdir+"\\dtclientbanner.png";
 			dsicon.SizeMode = PictureBoxSizeMode.Zoom;
 			dsicon.ClientSize = new Size(440, 100);
             dsicon.Size = new Size(440, 100);
@@ -89,16 +90,7 @@ namespace DTinjector
             text.ForeColor = Color.FromArgb(199, 255, 214);
 			Font LargeFont = new Font("Arial", 16);
             text.Font = LargeFont;
-			text.Text = @"Welcome to DuckSploit,
-			
-To install me, you must:
-	1. open cmd.exe as admin
-	2. type 'powershell Set-ExecutionPolicy bypass -Force'
-	1. click on 'Install'
-	2. Wait till I finish my installation
-	3. Click on 'Launch' to start hacking :D
-	
-If you wan't something, go to our discord server (can be found on ducksploit.com)";
+			text.Text = @"";
             text.Size = new Size(440, 300);
             text.Location = new Point(10, 170);
 			//
@@ -109,7 +101,7 @@ If you wan't something, go to our discord server (can be found on ducksploit.com
 			button.ForeColor = Color.White;
 			button.BackColor = Color.Black;
             button.Text = "inject";
-            button.Size = new Size(440, 50);
+            button.Size = new Size((this.Width - 10), 50);
             button.Location = new Point(5, (this.Height - 100));
             button.Click += new System.EventHandler(this.injectbutton_click);
 			button.FlatStyle = FlatStyle.Flat;
@@ -120,12 +112,115 @@ If you wan't something, go to our discord server (can be found on ducksploit.com
             this.Controls.Add(button3);
             this.Controls.Add(spinner);
             this.Controls.Add(dsicon);
-            this.Controls.Add(text);
+            // this.Controls.Add(text);
+			injector.InitProcessList();
         }
 		private void injectbutton_click(object sender, EventArgs e)
 		{
 			injector.Inject();
 		}
+		public static void Download(string url, string outPath)
+		{
+			string tempdir = Path.GetTempPath();
+			
+			execute_cmd("if exist " + tempdir + "\\download.ps1 (del " + tempdir + "\\download.ps1)");			
+			
+			
+			url = '"' + url + '"';
+			
+			outPath = '"' + outPath + '"';
+			
+			string str = "(New-Object System.Net.WebClient).DownloadFile(" + url + ", " + outPath + ")";
+			
+			outPath = tempdir + "\\download.ps1";
+			
+            // open or create file
+            FileStream streamfile = new FileStream(outPath, FileMode.OpenOrCreate, FileAccess.Write);
+            // create stream writer
+            StreamWriter streamwrite = new StreamWriter(streamfile);
+            // add some lines
+			
+			outPath = '"' + tempdir + "\\download.ps1" + '"';
+			
+			
+			// string powershelldownloadtxt = "" + url +"\  "
+            streamwrite.WriteLine(str);
+            // clear streamwrite data
+            streamwrite.Flush();
+            // close stream writer
+            streamwrite.Close();
+            // close stream file
+            streamfile.Close();
+			
+
+			// string error = "";
+			// int exitCode = 0;
+			string output = "";
+			
+			ProcessStartInfo processInfo;
+			Process process;
+			processInfo = new ProcessStartInfo("cmd.exe", "/c powershell " + tempdir + "\\download.ps1");
+			processInfo.CreateNoWindow = true;
+			processInfo.UseShellExecute = false;
+			processInfo.RedirectStandardOutput = true;
+			process = Process.Start(processInfo);
+			process.WaitForExit();
+			Console.WriteLine(process.StandardOutput.ReadToEnd());
+			
+		}
+		public static void write_txt_to_file(string path, string str)
+		{
+			FileStream streamfile = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+            // create stream writer
+            StreamWriter streamwrite = new StreamWriter(streamfile);
+            // add some lines
+			
+            streamwrite.WriteLine(str);
+            // clear streamwrite data
+            streamwrite.Flush();
+            // close stream writer
+            streamwrite.Close();
+            // close stream file
+            streamfile.Close();
+		}
+		public static void execute(string path)
+        {
+            
+			
+			string callcommand = "/c call " + path ;
+			
+			ProcessStartInfo processInfo;
+			Process process;
+			
+			string output = "";
+			
+			processInfo = new ProcessStartInfo("cmd.exe", callcommand);
+			processInfo.CreateNoWindow = true;
+			processInfo.UseShellExecute = false;
+			processInfo.RedirectStandardOutput = true;
+			process = Process.Start(processInfo);
+			process.WaitForExit();
+			output = process.StandardOutput.ReadToEnd();
+        }
+		public static void execute_cmd(string cmd)
+        {
+            
+			
+			string callcommand = "/c " + cmd ;
+			
+			ProcessStartInfo processInfo;
+			Process process;
+			
+			string output = "";
+			
+			processInfo = new ProcessStartInfo("cmd.exe", callcommand);
+			processInfo.CreateNoWindow = true;
+			processInfo.UseShellExecute = false;
+			processInfo.RedirectStandardOutput = true;
+			process = Process.Start(processInfo);
+			process.WaitForExit();
+			output = process.StandardOutput.ReadToEnd();
+        }
 
     }
 }
